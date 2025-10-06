1cd4d813bbfacafa137f78d7857b3c2f488a389b
Doboseriu Andrew <andreidoboseriu2008@gmail.com>
2025-10-06 17:39:24 +0300

    Added Permissions

--- src/main/java/me/andrew/joinLeaveMessage/JoinLeaveCommands.java
+++ src/main/java/me/andrew/joinLeaveMessage/JoinLeaveCommands.java
@@ -13,33 +13,41 @@ public class JoinLeaveCommands implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        String noPermissionMessage = "&cYou don't have permission to run this command!";
 
         //Join Message Command
         if(command.getName().equalsIgnoreCase("setjm")){
-            if(args.length == 0){
+            if(!sender.hasPermission("joinleavemessage.setjm")){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermissionMessage));
+                return true;
+            }
+            else if(args.length == 0){
                 sender.sendMessage("Usage: /setjm <message>");
                 return true;
             }
             else{
                 String newJoinMessage = String.join(" ", args);
-                String setMessage = "&dJoin Message updated successfully!";
+                String setMessage = "&aJoin Message updated successfully!";
                 plugin.getConfig().set("join-message", newJoinMessage);
                 plugin.saveConfig();
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', setMessage));
             }
-
             return true;
         }
 
         //Leave Message command
         else if(command.getName().equalsIgnoreCase("setlm")){
+            if(!sender.hasPermission("joinleavemessage.setlm")){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermissionMessage));
+                return true;
+            }
             if(args.length == 0){
                 sender.sendMessage("Usage: /setlm <message>");
                 return true;
             }
             else{
                 String newLeaveMessage = String.join(" ", args);
-                String setMessage = "&dLeave Message updated successfully!";
+                String setMessage = "&aLeave Message updated successfully!";
                 plugin.getConfig().set("leave-message", newLeaveMessage);
                 plugin.saveConfig();
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', setMessage));
